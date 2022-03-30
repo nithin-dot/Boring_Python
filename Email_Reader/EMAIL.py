e15e069a69ddc400eccfb3c8def56195242eeca6
nithin-dot <Nithinkirthick@gmail.com>
2022-03-31 01:03:14 +0530

    minnor changes

--- Email_Reader/EMAIL.py
+++ Email_Reader/EMAIL.py
@@ -13,8 +13,8 @@ import time
 
 import datetime    
 host = 'imap.gmail.com' # mail api address
-username = 'Nithinkirthick.it19@bitsathy.ac.in'# enter your mail address
-password = 'Nithin@2002' # enter your password
+username = 'Example@gmail.com'# enter your mail address
+password = 'password' # enter your password
 cleaner = re.compile('<.*?>') #clear the tags from mail
 cleaner_2 = re.compile('=.*?=')
 byte=[] # array declare
@@ -42,15 +42,15 @@ def get_inbox():#function
             frommail = re.sub(cleaner, '',email_message[From]) # rm the unwanted char
             if("Gmail Team" in frommail):
                 continue
-            elif("GOKULAKRISHNAN D" in frommail):
+            elif("Team Name" in frommail):
                 continue
-            elif("MADHUSUDHANAN G" in frommail):
+            elif("Team Name" in frommail):
                 continue
-            elif("BALAKUMAR M" in frommail):
+            elif("Team Name" in frommail):
                 continue
-            elif("SHRI THARANYAA J P BIT" in frommail):
+            elif("Team Name" in frommail):
                 continue
-            elif("TAMILSELVAN S" in frommail):
+            elif("Team Name" in frommail):
                 continue
             mail.store(num, "+FLAGS", "\\Deleted")
             print(f'\nDeleted From {frommail}at {email_message[Date]}') #format the input
